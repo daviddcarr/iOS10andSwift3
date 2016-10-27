572c6cd7cd895f01265ec17765c380b43034431a
David Carr <david@yellow7.com>
2016-10-27 09:01:23 -0500

    Add content for Section 4, Lecture 63 - Band App

---
+++ Apps/MusicianFanApp/MusicianFanApp/MainVC.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MusicianFanApp
+//
+//  Created by David Carr on 10/27/16.
+//  Copyright © 2016 carrtoonist. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
